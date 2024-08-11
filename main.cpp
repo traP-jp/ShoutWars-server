a696c46549ac43ae8ebf5037a81b229cd63d28f9
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2024-08-12 05:28:31 +0900

    Initialize HTTP API server

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+#include <nlohmann/json.hpp>
+#include <httplib.h>
+
+using json = nlohmann::json;
+using namespace httplib;
+using namespace std;
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  Server server;
+
+  server.Get("/", [](const Request& req, Response& res) {
+    json response;
+    response["message"] = "Hello, World!";
+    res.set_content(response.dump(), "application/json");
+    cout << "GET /\n" << response.dump() << "\n" << endl;
+  });
+
+  cout << "Server started at http://localhost:7468" << endl;
+  server.listen("localhost", 7468);
+
   return 0;
 }
