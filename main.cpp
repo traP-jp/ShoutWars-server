8456e3b2472f15ff420654c7f106d0f4d400eea6
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2024-08-13 00:05:32 +0900

    Add Boost JSONv7 generator

--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
 #include <nlohmann/json.hpp>
 #include <httplib.h>
+#include <boost/uuid/time_generator_v7.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <iostream>
 
-using json = nlohmann::json;
+using namespace nlohmann;
 using namespace httplib;
+using namespace boost::uuids;
 using namespace std;
 
 int main() {
+  time_generator_v7 id_gen;
   Server server;
 
-  server.Get(".*", [](const Request& req, Response& res) {
+  server.Get(".*", [&](const Request& req, Response& res) {
     json response;
     response["path"] = req.path;
+    response["id"] = to_string(id_gen());
     res.set_content(response.dump(), "application/json");
     cout << "GET " << req.path << "\n" << response.dump() << "\n" << endl;
   });
