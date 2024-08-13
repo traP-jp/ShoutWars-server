ef4e7a15c1698d34cbad871b08d0a4396ede7637
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2024-08-13 23:41:54 +0900

    Add error handling

--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
+#include "room.hpp"
+
 #include <nlohmann/json.hpp>
 #include <httplib.h>
-#include <boost/uuid/time_generator_v7.hpp>
 #include <boost/uuid/uuid_io.hpp>
 #include <iostream>
 #include <format>
 #include <string>
+#include <functional>
+#include <exception>
 #include <cstdlib>
 
-using namespace nlohmann;
-using namespace httplib;
-using namespace boost::uuids;
 using namespace std;
+
+using json = nlohmann::json;
+using Server = httplib::Server;
+using Request = httplib::Request;
+using Response = httplib::Response;
 
 string getenv_or(const string &key, const string &default_value) {
   const char *value = getenv(key.c_str());
@@ -24,46 +29,69 @@ const int port = stoi(getenv_or("PORT", "7468"));
 const string password = getenv_or("PASSWORD", "");
 const int room_limit = stoi(getenv_or("ROOM_LIMIT", "100"));
 
-void responseMsgpack(Response &res, const json &data) {
-  const auto msgpack = json::to_msgpack(data);
-  res.set_content(string(msgpack.begin(), msgpack.end()), "application/msgpack");
-}
+void log_stdout(const string &msg) { cout << msg << endl; }
+void log_stderr(const string &msg) { cerr << msg << endl; }
 
-bool check_password(const Request &req, Response &res) {
-  if (password.empty() || req.get_header_value("Authorization") == "Bearer "s + password) return false;
-  res.status = 404;
-  return true;
-}
-
-void fallback_version(Server &server) {
-  const string pattern = format(R"((?!{}/).*)", api_path);
-  const Server::Handler handler = [&](const Request &req, Response &res) {
-    if (check_password(req, res)) return;
-    res.status = 404;
-    responseMsgpack(res, {
-      { "error", format("Invalid API version. Use {}.", api_path) },
-    });
+auto gen_auth_handler(const function<json(const json &)> &handle_json) {
+  return [&](const Request &req, Response &res) {
+    if (!password.empty() && req.get_header_value("Authorization") != "Bearer "s + password) {
+      res.status = 404;
+      return;
+    }
+    try {
+      const auto msgpack = json::to_msgpack(
+        handle_json(req.body.empty() ? json(nullptr) : json::from_msgpack(req.body))
+      );
+      res.set_content(string(msgpack.begin(), msgpack.end()), "application/msgpack");
+    } catch (const json::exception &err) {
+      throw pair<int, string>(400, err.what());
+    }
   };
-  server.Get(pattern, handler);
-  server.Post(pattern, handler);
 }
 
 int main() {
-  time_generator_v7 id_gen;
+  room_list_t room_list(log_stdout, log_stderr, room_limit);
+
   Server server;
 
-  server.Get(api_path + "/status"s, [&](const Request &req, Response &res) {
-    if (check_password(req, res)) return;
-    responseMsgpack(res, {
-      { "room_count", 0 },
-      { "room_limit", room_limit },
-    });
-  });
+  server.set_exception_handler(
+    [&](const Request &req, Response &res, const exception_ptr &ep) {
+      try {
+        rethrow_exception(ep);
+      } catch (const pair<int, string> &err) {
+        res.status = err.first;
+        const auto msgpack = json::to_msgpack(json{ { "error", err.second } });
+        res.set_content(string(msgpack.begin(), msgpack.end()), "application/msgpack");
+      } catch (const exception &err) {
+        res.status = 500;
+        log_stderr(format("Internal server error: {}\n  when {} {}", err.what(), req.method, req.path));
+      } catch (...) {
+        res.status = 500;
+        log_stderr(format("Unknown error\n  when {} {}", req.method, req.path));
+      }
+    }
+  );
 
-  fallback_version(server);
+  const string invalid_ver_pattern = format(R"((?!{}/).*)", api_path);
+  const Server::Handler invalid_ver_handler = gen_auth_handler(
+    [&](const json &req)-> json {
+      return { { "error", format("Invalid API version. Use {}.", api_path) } };
+    }
+  );
+  server.Get(invalid_ver_pattern, invalid_ver_handler);
+  server.Post(invalid_ver_pattern, invalid_ver_handler);
 
-  cout << format("Server started at http://localhost:{}", port) << endl;
-  if (!password.empty()) cout << format("Password: {}", password) << endl;
+  server.Get(
+    api_path + "/status"s,
+    gen_auth_handler(
+      [&](const json &req) -> json {
+        return { { "room_count", room_list.rooms.size() }, { "room_limit", room_list.limit } };
+      }
+    )
+  );
+
+  log_stdout(format("Server started at http://localhost:{}", port));
+  if (!password.empty()) log_stdout(format("Password: {}", password));
   server.listen("0.0.0.0", port);
 
   return 0;
