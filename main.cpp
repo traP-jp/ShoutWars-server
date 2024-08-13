35805d6879ca3a6bfd5599dbdeb8a6f1a28a8f51
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2024-08-13 09:16:59 +0900

    Add status endpoint

--- main.cpp
+++ main.cpp
@@ -3,26 +3,59 @@
 #include <boost/uuid/time_generator_v7.hpp>
 #include <boost/uuid/uuid_io.hpp>
 #include <iostream>
+#include <format>
+#include <string>
+#include <cstdlib>
 
 using namespace nlohmann;
 using namespace httplib;
 using namespace boost::uuids;
 using namespace std;
 
+string getenv_or(const string &key, const string &default_value) {
+  const char *value = getenv(key.c_str());
+  return value ? value : default_value;
+}
+
+constexpr int api_ver = 0;
+const string api_path(format("/v{}", api_ver));
+
+const int port = stoi(getenv_or("PORT", "7468"));
+const string password = getenv_or("PASSWORD", "");
+const int room_limit = stoi(getenv_or("ROOM_LIMIT", "100"));
+
+void responseMsgpack(Response &res, const json &data) {
+  const auto msgpack = json::to_msgpack(data);
+  res.set_content(string(msgpack.begin(), msgpack.end()), "application/msgpack");
+}
+
+void fallback_version(Server &server) {
+  const string pattern = format(R"((?!{}/).*)", api_path);
+  const Server::Handler handler = [&](const Request &req, Response &res) {
+    res.status = 404;
+    responseMsgpack(res, {
+      { "error", format("Invalid API version. Use {}.", api_path) },
+    });
+  };
+  server.Get(pattern, handler);
+  server.Post(pattern, handler);
+}
+
 int main() {
   time_generator_v7 id_gen;
   Server server;
 
-  server.Get(".*", [&](const Request& req, Response& res) {
-    json response;
-    response["path"] = req.path;
-    response["id"] = to_string(id_gen());
-    res.set_content(response.dump(), "application/json");
-    cout << "GET " << req.path << "\n" << response.dump() << "\n" << endl;
+  server.Get(api_path + "/status"s, [&](const Request &req, Response &res) {
+    responseMsgpack(res, {
+      { "room_count", 0 },
+      { "room_limit", room_limit },
+    });
   });
 
-  cout << "Server started at http://localhost:7468" << endl;
-  server.listen("0.0.0.0", 7468);
+  fallback_version(server);
+
+  cout << format("Server started at http://localhost:{}", port) << endl;
+  server.listen("0.0.0.0", port);
 
   return 0;
 }
