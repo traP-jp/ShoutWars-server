baab55f512b4518dc955f356837547eb5b42f51c
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2024-08-17 19:33:26 +0900

    Add final to errors

--- errors.hpp
+++ errors.hpp
@@ -9,32 +9,32 @@ public:
   explicit error(int code, const std::string &what) : runtime_error(what), code(code) {}
 };
 
-class bad_request_error : public error {
+class bad_request_error final : public error {
 public:
   explicit bad_request_error(const std::string &what) : error(400, what) {}
 };
 
-class unauthorized_error : public error {
+class unauthorized_error final : public error {
 public:
   explicit unauthorized_error(const std::string &what) : error(401, what) {}
 };
 
-class forbidden_error : public error {
+class forbidden_error final : public error {
 public:
   explicit forbidden_error(const std::string &what) : error(403, what) {}
 };
 
-class not_found_error : public error {
+class not_found_error final : public error {
 public:
   explicit not_found_error(const std::string &what) : error(404, what) {}
 };
 
-class internal_server_error : public error {
+class internal_server_error final : public error {
 public:
   explicit internal_server_error(const std::string &what) : error(500, what) {}
 };
 
-class service_unavailable_error : public error {
+class service_unavailable_error final : public error {
 public:
   explicit service_unavailable_error(const std::string &what) : error(503, what) {}
 };
